5c48e001c3464bbe9c9fb3636897a924a62de33d
feralonsomaccari <feralonsomaccari@gmail.com>
2020-03-23 19:46:03 -0300

    Update Experience section

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
-new WOW().init();
+new WOW(
+  {
+    boxClass:     'wow',      // default
+    animateClass: 'animated', // default
+    offset:       0,          // default
+    mobile:       false,       // default
+    live:         true        // default
+    }
+).init();
 
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 particlesJS.load('particles-js', 'js/particles.js-master/particles.json', function() {
